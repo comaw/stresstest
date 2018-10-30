980b1dda91ffcaf4ddd15fff8ec9bd0785928014
S.Yanchevsky <s.yanchevsky@spheremall.com>
2018-10-30 17:57:49 +0200

    Fix listing

--- lib/strain.go
+++ lib/strain.go
@@ -16,15 +16,16 @@ type Strain struct {
 	Listing   []string
 }
 
-func (strain *Strain) Get(w http.ResponseWriter, streamsNumber int) bool {
+func (strain *Strain) Get(w http.ResponseWriter, streamsNumber int, ittr int) bool {
 
 	timeStart := time.Now()
 	averageTime := 0.0
 	maxMin := make([]float64, 2)
 	maxMin[1] = 100.12
 	countOfHttpErrors := 0
+	listOfOll := make([]float64, ittr)
 
-	for i := 0; i <= strain.Itt; i++ {
+	for i := 0; i < strain.Itt; i++ {
 		timeStart = time.Now()
 		_, httpErr := http.Get(strain.Url)
 		if httpErr != nil {
@@ -38,11 +39,23 @@ func (strain *Strain) Get(w http.ResponseWriter, streamsNumber int) bool {
 		if maxMin[1] > timeStop {
 			maxMin[1] = timeStop
 		}
+		listOfOll[i] = timeStop
 	}
 	averageTime = averageTime / float64(strain.Itt)
+	listMinMax := make([]float64, 2)
+	for _, element := range listOfOll {
+		if element >= averageTime {
+			listMinMax[0] += 1.0
+		}
+		if element < averageTime {
+			listMinMax[1] += 1.0
+		}
+	}
 	httpMessage := "Iteration: " + strconv.Itoa(strain.Itt) + " | Average Time: " + strconv.FormatFloat(averageTime, 'f', 6, 64)
 	httpMessage += " | Max Time: " + strconv.FormatFloat(maxMin[0], 'f', 6, 64)
 	httpMessage += " | Min Time: " + strconv.FormatFloat(maxMin[1], 'f', 6, 64)
+	httpMessage += " | Min Count: " + strconv.FormatFloat(listMinMax[1], 'f', 6, 64)
+	httpMessage += " | Max Count: " + strconv.FormatFloat(listMinMax[0], 'f', 6, 64)
 	httpMessage += " | Count of http errors: " + strconv.Itoa(countOfHttpErrors)
 	strain.Listing[streamsNumber] = httpMessage
 	strain.Finishing += 1
@@ -62,7 +75,7 @@ func (strain *Strain) Requests(w http.ResponseWriter) bool {
 	for i := 0; i < strain.Streams; i++ {
 		switch strain.Method {
 		case "GET":
-			go strain.Get(w, i)
+			go strain.Get(w, i, strain.Itt)
 			break
 		default:
 			fmt.Fprintf(w, streamsInt)
